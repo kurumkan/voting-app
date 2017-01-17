4ae898735f792f2f3a1773139ef73a997884fd69
kurumkan <joomrise@gmail.com>
2017-01-17 22:33:12 +0800

    refactor

--- webpack.config.js
+++ webpack.config.js
@@ -11,12 +11,10 @@ module.exports = {
   },
   plugins: [
   new webpack.DefinePlugin({
-  "process.env": { 
-     NODE_ENV: JSON.stringify("production") 
-   }
-})
-,
-  
+    "process.env": { 
+       NODE_ENV: JSON.stringify("production") 
+      }
+    }),  
     new webpack.ProvidePlugin({
       '$': 'jquery',
       'jQuery': 'jquery'
